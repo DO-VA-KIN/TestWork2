8d8b42e3ba401a5e3906b1a82981883726102a7a
ghost <--->
2023-03-09 17:21:41 +0300

    --

---
+++ Serealizer_/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace Serealizer_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListRandom listRandom = new ListRandom();
+
+            Example ex = new Example();
+            listRandom.Head = ex.Root;
+            listRandom.Tail = ex.Tail;
+            listRandom.Count = ex.Count;
+
+            using (FileStream stream = File.Open(Environment.CurrentDirectory + @"\serealize.txt", FileMode.Create))
+                listRandom.Serealize(stream);
+
+           // using (Stream stream = File.Open(Environment.CurrentDirectory + @"\serealize.txt", FileMode.Open))
+                //listRandom.Deserealize(stream);
+
+        }
+
+
+
+
+
+    }
+
+    public class Example
+    {
+        public Example()
+        {
+            ListNode node1 = new ListNode();
+            ListNode node2 = new ListNode();
+            ListNode node3 = new ListNode();
+            ListNode node4 = new ListNode();
+            ListNode node5 = new ListNode();
+
+            node1.Previous = null;
+            node1.Next = node2;
+            node1.Random = null;
+            node1.Data = "AB";
+
+            node2.Previous = node1;
+            node2.Next = node3;
+            node2.Random = node3;
+            node2.Data = "BC";
+
+            node3.Previous = node2;
+            node3.Next = node4;
+            node3.Random = node5;
+            node3.Data = "CD";
+
+            node4.Previous = node3;
+            node4.Next = node5;
+            node4.Random = node1;
+            node4.Data = "DE";
+
+            node5.Previous = node4;
+            node5.Next = null;
+            node5.Random = node1;
+            node5.Data = "EF";
+
+            Root = node1;
+            Tail = node5;
+            Count = 5;
+        }
+
+        public ListNode Root;
+        public ListNode Tail;
+        public int Count;
+    }
+}
+
---
+++ Serealizer_/Serealizer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Serealizer_
+{
+    public class ListNode
+    {
+        public ListNode Previous;
+        public ListNode Next;
+        public ListNode Random;
+
+        public string Data;
+    }
+
+    class ListRandom
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serealize(FileStream s)
+        {
+            int num = -1;
+            Encoding coder = Encoding.UTF8;
+            //s.Write(coder.GetBytes(count));
+
+            ListNode current = Head;
+            while(current != null)
+            {
+                ListNode current2 = Head;
+                for (int i = 0; current2 != null; i++)
+                {
+                    if (current.Random == current2)
+                    {
+                        num = i;
+                        break;
+                    }
+                    current2 = current2.Next;
+                }
+
+                string w;
+                if (num > -1)
+                    w = current.Data +"<" + num +">\n";
+                else
+                    w = current.Data + "\n";
+
+                s.Write(coder.GetBytes(w));
+                current = current.Next;
+            }
+        }
+
+        public void Deserealize(Stream s)
+        {
+            //byte[] buff = new byte[s.Length]; //если большой объём данных не предполагается, то лучше считать всё сразу для большей скорости(или искать баланс загрузки оперативки и жесткого диска)
+            //s.Read(buff);                     //но полагаю, задачи оптимизации в данном случае нет
+            ListNode lastPrevious = null;
+            byte[] buff = new byte[1];
+            Encoding coder = Encoding.UTF8;
+
+            ListNode current = Head = new ListNode();
+            while (s.Position < s.Length)
+            {
+                string r = null;
+                string r2 = null;
+
+                s.Read(buff);
+                char ch = BitConverter.ToChar(buff);
+                while (ch != '<')
+                {
+                    if (ch == '\n') break;
+                    r += ch;
+                    s.Read(buff);
+                    ch = BitConverter.ToChar(buff);
+                }
+                s.Read(buff);
+                BitConverter.ToChar(buff);
+                while (ch != '>')
+                {
+                    if (ch == '\n') break;
+                    r2 += ch;
+                    s.Read(buff);
+                    BitConverter.ToChar(buff);
+                }
+
+                current.Data = r;
+
+                if (current != Head)
+                    current.Previous = lastPrevious;
+                lastPrevious = current;
+                
+                if (r2 != null)
+                {
+
+                }
+
+
+            }
+        }
+    }
+   
+}
+
